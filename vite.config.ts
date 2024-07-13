251dff2a077dc907abd1beef3c4d3df067e5ac85
Aleksandr waitstop Kazakov <kazakov2003931@yandex.ru>
2024-07-13 10:55:49 +0300

    vite.config.ts changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: import.meta.env.PROD ? '/browser-start-page/' : '/',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/browser-start-page/' : '/',
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
